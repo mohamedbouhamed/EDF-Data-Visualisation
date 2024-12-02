1f4796dc7b6a7d8eb383412a0d39511dbfc03aab
Bouhamed Mohamed <mohamed.bouhamed@student-cs.fr>
2024-12-02 16:29:43 +0100

    02/12/24

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CourseCardComponent } from '../course-card/course-card.component';
 import { DataSetResult, DataSets, ICourse, courses } from '../app.component.models';
 import { CommonModule } from '@angular/common';
-import { CatalogService } from '../srvices/catalog.service';
+import { CatalogService} from '../srvices/catalog.service';
+import { DatasetService } from '../srvices/dataset.service';
 
 @Component({
   selector: 'app-course-list',
@@ -15,16 +16,23 @@ export class CourseListComponent {
   courses: ICourse[] = courses;
   /* datasets: Array<any> = []; */
   datasets: DataSetResult[] = [];
+  dataset1 : any[] = [];
   title='hello';
   onCourseClicked(course:ICourse): void{
     console.log('courseclicked ',course.description);
   }
     //mathod 1 to call data from service
-    constructor(private catalogService: CatalogService){};
+    constructor(private catalogService: CatalogService, private datasetService: DatasetService){};
     ngOnInit(): void {
       this.catalogService.getDatasets().subscribe((data: DataSets) => {
         this.datasets = data.results; // Récupérer les résultats depuis l'objet DataSets
         console.log('datasets ', this.datasets);
+      });
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, '0'); // Heure avec deux chiffres
+      this.datasetService.getDatasetAllRecords("disponibilite-du-parc-nucleaire-d-edf-sa-present-passe-et-previsionnel").subscribe((data: any) => {
+        this.dataset1 = data; // Récupérer les résultats depuis l'objet DataSets
+        console.log('datasets ', this.dataset1);
       });
     }  
     /* ngOnInit(): void {
--- src/app/srvices/dataset.service.ts
+++ src/app/srvices/dataset.service.ts
@@ -1,16 +1,21 @@
 import { Injectable, inject } from '@angular/core';
-import{ HttpClient, HttpErrorResponse} from '@angular/common/http';
+import{ HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const baseUrl = 'https://opendata.edf.fr/api/explore/v2.1/catalog/datasets'
 @Injectable({
   providedIn: 'root'
 })
-export class DatasetsService {
+export class DatasetService {
   http = inject(HttpClient)
   getDatasetAllRecords(dataset_id: string): Observable<any>{
-    return this.http.get(`${baseUrl}/datasets/${dataset_id}/records`)
-  }
+      const params = new HttpParams()
+        .set('limit', '100')
+        .set('offset', '0')
+        .set('include_links', 'false')
+        .set('include_app_metas', 'false');
+    return this.http.get(`${baseUrl}/${dataset_id}/records`, {params})
+  };
   getDatasetExportFormats(dataset_id: string): Observable<any>{
     return this.http.get(`${baseUrl}/datasets/${dataset_id}/exports`)
   }
