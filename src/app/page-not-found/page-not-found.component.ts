80668afad98c2fe2429606f8764625ebb652f26f
Bouhamed Mohamed <mohamed.bouhamed@student-cs.fr>
2025-01-08 22:09:31 +0100

    4e essai de deploiement

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
 export class PageNotFoundComponent {
-
+  constructor(private router: Router) {}
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
